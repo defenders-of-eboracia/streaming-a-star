cf7703932cc90bb03c63a0b41e738df1f1a6c0b3
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2017-02-23 20:34:32 +0000

    We got something

--- graph.py
+++ graph.py
@@ -1,27 +1,54 @@
+from evaluate import score as get_score
 
 
-class Node:
-    """
-    Represent a node in the state of the game.
-    """
+def _get_caches_contents_by_action(state, action):
+    contents = state.caches_contents[:]
+    current_size, videos = contents[action.cache_id]
+    current_size += action.video_size
+    videos = videos[:]
+    videos.append(action.video_id)
+    contents[action.cache_id] = (current_size, videos)
+    return contents
 
-    def __init__(self, score, caches_contents):
-        """
-        Represent the a possible state, given the state of the caches.
-        :param caches_contents: A list of tuples (cache_current_size, [video_id,...])
-        """
+
+def _clean_solution(solution):
+    return list([video_ids for (capacity, video_ids) in solution])
+
+
+class MazeAction(object):
+    def __init__(self, cache_id, video_id, video_size):
+        self.cache_id = cache_id
+        self.video_id = video_id
+        self.video_size = video_size
+        self.reward = None
+
+    def compute_reward(self, problem, previous_state, base_score):
+        contents = _get_caches_contents_by_action(state=previous_state, action=self)
+        self.reward = get_score(problem, _clean_solution(contents))
+
+    def __eq__(self, other):
+        return self.cache_id == other.cache_id and self.video_id == other.video_id
+
+    def __hash__(self):
+        return self.cache_id * 100000 + self.video_id
+
+
+class MazeState(object):
+
+    def __init__(self, score, caches_contents, problem):
         self.caches_contents = caches_contents
         self.score = score
+        self.problem = problem
+        self.actions = self._get_actions()
 
-    def options(self, problem):
+    def _get_actions(self):
         """
         Get options after having added a single
-        :param problem: The problem descriptor.
-        :return: A list of (cost, node).
+        :return: A list of actions
         """
 
         # The max capacity of each cache server
-        max_capacity = problem.cacheCapacity
+        max_capacity = self.problem.cacheCapacity
 
         options = []
 
@@ -29,27 +56,46 @@ class Node:
         for cache_id, (cache_current_size, cache_videos) in enumerate(self.caches_contents):
 
             # For each video we can add
-            for video_id, video_size in enumerate(problem.videoSizes):
+            for video_id, video_size in enumerate(self.problem.videoSizes):
 
+                # Don't overfill the cache (capacity)
                 if video_size + video_size > max_capacity:
                     continue
 
-                cost_function = lambda problem, solution: -1
+                # Don't duplicate videos in a cache!
+                if video_id in self.caches_contents[cache_id][1]:
+                    continue
 
-                # Create a copy of this node with the cache having this video in it.
-                contents = self.caches_contents
-                contents[cache_id][0] += video_size
-                contents[cache_id][1].append(video_id)
-                score = cost_function(problem, contents)
-                node = Node(contents, score)
-
-                # The weight of the connection is the difference in score (negative, as obj. is minimisation)
-                weight = -(score - self.score)
-                option = (weight, node)
-
-                options.append(option)
+                action = MazeAction(cache_id=cache_id, video_id=video_id,
+                                    video_size=video_size)
+                action.compute_reward(self.problem, previous_state=self, base_score=self.score)
+                options.append(action)
 
         return options
 
+    def perform(self, action):
+
+        contents = _get_caches_contents_by_action(self, action)
+        new_score = self.score + action.reward
+
+        node = MazeState(score=new_score, caches_contents=contents,
+                         problem=self.problem)
+
+        return node
+
+    def reward(self, parent, action):
+        return action.reward
+
+    def is_terminal(self):
+        return len(self.actions) == 0
+
+    def __eq__(self, other):
+        return self.caches_contents == other.caches_contents
+
     def __hash__(self):
-        return hash(self.__repr__())
+        return str(self.caches_contents).__hash__()
+
+
+
+
+
--- script.py
+++ script.py
@@ -1,10 +1,15 @@
 from FileReader import FileReader
+from mcts.mcts import MCTS
+from mcts.tree_policies import UCB1
+from mcts.default_policies import immediate_reward
+from mcts.backups import monte_carlo
+from mcts.graph import StateNode
 
 
 # Read the input file
-from graph import Node
+from graph import MazeState, MazeAction
 
-testfile = "small.in"
+testfile = "trivialExample.in"
 problem = FileReader(testfile)
 print("Video Sizes: %r" % (problem.videoSizes,))
 print("Endpoints:\n\t%s" % ("\n\t".join([str(e) for e in problem.endpoints])))
@@ -13,8 +18,14 @@ print("Requests: %r" % ([r for r in problem.requests]))
 # Generate initial state
 initial_contents = list([(0, []) for _ in range(problem.nCaches)])
 initial_score = 0
-initial_state = Node(caches_contents=initial_contents, score=initial_score)
+initial_state = MazeState(caches_contents=initial_contents, score=initial_score,
+                          problem=problem)
 
 # Generate the optimal end state
+mcts = MCTS(tree_policy=UCB1(c=1.41),
+            default_policy=immediate_reward,
+            backup=monte_carlo)
 
-# TODO A-Star needs end-node!! Fuck!!
+root = StateNode(parent=None, state=initial_state)
+best_action = mcts(root)
+print(best_action)
